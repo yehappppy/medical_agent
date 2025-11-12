9ea7bc523a3d5dab5b6b28f917db122a0a8af66c
yehappppy <yehappppy@gmail.com>
2025-11-13 02:32:58 +0800

    [Add] Embedding model client

--- agent/utils/chunker.py
+++ agent/utils/chunker.py
@@ -1,1 +1,12 @@
+from langchain_openai import OpenAIEmbeddings
+from pydantic import SecretStr
 
+
+def get_embedding_model(base_url: str, api_key: SecretStr,
+                        model_name: str) -> OpenAIEmbeddings:
+    embedding_model = OpenAIEmbeddings(
+        model=model_name,
+        base_url=base_url,
+        api_key=api_key,
+    )
+    return embedding_model
