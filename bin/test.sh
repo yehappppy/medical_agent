4f4437a128a8840ebe0afcf8b2f8ecf1b44414fe
yehappppy <yehappppy@gmail.com>
2025-11-15 17:27:17 +0800

    [Update] Modify project structure

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,4 @@ MODULE_HOME="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 set -a && source "$MODULE_HOME/conf/agent.conf" && set +a
 
-python "$MODULE_HOME/test/test_models.py"
+python "$MODULE_HOME/test.py"
